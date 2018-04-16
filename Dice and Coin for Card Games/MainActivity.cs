2f1a48846a585afee0414d7041ea6f56ca10cb61
Frankie Dickinson <kiwifrankiedox@gmail.com>
2018-04-16 15:57:03 +1200

    Add project files.

---
+++ Dice and Coin for Card Games/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Dice_and_Coin_for_Card_Games
+{
+    [Activity(Label = "Dice_and_Coin_for_Card_Games", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
